5fc7c7f4664bce54ffa3b4f29919e4661027e04d
imshiva17 <shivarajak277@gmail.com>
2026-05-25 23:49:10 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello server");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
