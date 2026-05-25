dfed6988b1d1d27ac4c4edd413b0a848d14e5c90
imshiva17 <shivarajak277@gmail.com>
2026-05-26 00:35:18 +0530

    Setup Server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./src/config/connectDB.js";
 dotenv.config();
 
 const app = express();
@@ -10,5 +11,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on the port ${PORT}`);
+  connectDB();
 });
