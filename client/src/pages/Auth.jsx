aeecea57297885af920c11555543916ee143de39
imshiva17 <shivarajak277@gmail.com>
2026-06-02 22:38:11 +0530

    auth.jsx file created

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Auth = () => {
   return (
     <div className="min-h-screen overflow-hidden bg-white text-black px-8 ">
-      
+      <div>
+        
+      </div>
     </div>
   );
 };
