f2e8ec221754ff8d033197d9831c37cc5b5e7bff
imshiva17 <shivarajak277@gmail.com>
2026-06-06 21:26:45 +0530

    Auth Page

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const Auth = () => {
   return (
-    <div className="min-h-screen overflow-hidden bg-white text-black px-8 ">
+    // <div className="min-h-screen overflow-hidden bg-white text-black px-8 ">
+      
+    // </div>
+    <div>
       
     </div>
   );
