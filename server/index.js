a9f177200f42b5417f927a97b5bdd239665dce80
imshiva17 <shivarajak277@gmail.com>
2026-06-21 20:48:12 +0530

    Google Auth completed

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./src/config/connectDB.js";
+import authRouter from "./src/routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello server");
-});
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
