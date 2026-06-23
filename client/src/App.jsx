ccfe94d64309a6269bf0f29eb23f701481bf45bb
imshiva17 <shivarajak277@gmail.com>
2026-06-23 23:07:49 +0530

    Next Home Page 2.37

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,25 @@
 import React, { useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
 import { getCurrentUser } from "./services/api";
+import { useDispatch, useSelector } from "react-redux";
 
 export const ServerUrl = "http://localhost:8000";
 
 const App = () => {
+  const dispatch = useDispatch();
   useEffect(() => {
-    getCurrentUser();
-  },[]);
+    getCurrentUser(dispatch);
+  }, [dispatch]);
+
+  const {userData} = useSelector((state)=>state.user)  
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<Auth />} />
+        <Route path="/" element={userData ? <Home /> : <Navigate to = "/auth" replace/> } />
+        <Route path="/auth" element={userData ? <Navigate to = "/" replace/> :<Auth />} />
       </Routes>
     </>
   );
--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 import { ServerUrl } from "../App";
+import { setUserData } from "../redux/userSlice";
 
-export const getCurrentUser = async () => {
+export const getCurrentUser = async (dispatch) => {
   try {
     const result = await axios.get(ServerUrl + "/api/user/currentuser", {
       withCredentials: true,
     });
-    console.log(result.data);
+
+    dispatch(setUserData(result.data));
   } catch (error) {
     console.log(error);
   }
