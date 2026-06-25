1ada7ea0429e8c62414e8ddbd00a2bd08564ae41
imshiva17 <shivarajak277@gmail.com>
2026-06-25 21:17:25 +0530

    Added Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,9 @@ import Home from "./pages/Home";
 import Auth from "./pages/Auth";
 import { getCurrentUser } from "./services/api";
 import { useDispatch, useSelector } from "react-redux";
+import History from "./pages/History";
+import Notes from "./pages/Notes";
+import Pricing from "./pages/Pricing";
 
 export const ServerUrl = "http://localhost:8000";
 
@@ -13,13 +16,31 @@ const App = () => {
     getCurrentUser(dispatch);
   }, [dispatch]);
 
-  const {userData} = useSelector((state)=>state.user)  
+  const { userData } = useSelector((state) => state.user);
 
   return (
     <>
       <Routes>
-        <Route path="/" element={userData ? <Home /> : <Navigate to = "/auth" replace/> } />
-        <Route path="/auth" element={userData ? <Navigate to = "/" replace/> :<Auth />} />
+        <Route
+          path="/"
+          element={userData ? <Home /> : <Navigate to="/auth" replace />}
+        />
+        <Route
+          path="/auth"
+          element={userData ? <Navigate to="/" replace /> : <Auth />}
+        />
+        <Route
+          path="/history"
+          element={userData ? <History /> : <Navigate to="/auth" replace />}
+        />
+        <Route
+          path="/notes"
+          element={userData ? <Notes /> : <Navigate to="/auth" replace />}
+        />
+        <Route
+          path="/pricing"
+          element={userData ? <Pricing /> : <Navigate to="/auth" replace />}
+        />
       </Routes>
     </>
   );
