889f7147d8f465cb24b9c60978692f4156cf9480
imshiva17 <shivarajak277@gmail.com>
2026-06-25 21:21:35 +0530

    Done Some Changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -77,7 +77,10 @@ const Navbar = () => {
                   Use credits to generate AI notes, diagrams & PDFs.
                 </p>
                 <button
-                  onClick={() => setShowCredits(false)}
+                  onClick={() => {
+                    setShowCredits(false);
+                    navigate("/pricing");
+                  }}
                   className="w-full py-2 rounded-lg bg-linear-to-br  from-white to-gray-200 text-black font-semibold hover:opacity-90 "
                 >
                   Buy More Credits
@@ -112,7 +115,10 @@ const Navbar = () => {
               >
                 <MenuItem
                   text="History"
-                  onClick={() => setShowProfile(false)}
+                  onClick={() => {
+                    setShowProfile(false);
+                    navigate("/history");
+                  }}
                 />
                 <div className="h-px bg-white/10 mx-3" />
                 <MenuItem text="Sign Out" red onClick={handleSignOut} />
